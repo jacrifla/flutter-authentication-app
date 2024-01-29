91a8b826798333a33d406d3aafb6127d7604eadb
jacrifla <jaquelini_cris@hotmail.com>
2024-01-29 20:34:52 -0300

    comentarios

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:textfield/constants/app_colors.dart';
