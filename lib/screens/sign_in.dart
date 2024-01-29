674c2aa76a30282cee4c04eec932f669e5868762
jacrifla <jaquelini_cris@hotmail.com>
2024-01-29 20:38:24 -0300

    colocado a logica para validacao dos inputs

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -1,20 +1,30 @@
+// ignore_for_file: prefer_const_constructors, use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:textfield/constants/app_colors.dart';
 import 'package:textfield/widgets/footer.dart';
 import 'package:textfield/widgets/big_title.dart';
 
+import '../utils/utils.dart';
 import '../widgets/button.dart';
 import '../widgets/input.dart';
 
 class SignIn extends StatefulWidget {
-  const SignIn({super.key});
+  const SignIn({Key? key}) : super(key: key);
 
   @override
   State<SignIn> createState() => _SignInState();
 }
 
 class _SignInState extends State<SignIn> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  String? _emailErrorText;
+  String? _passwordErrorText;
+  bool _isLoading = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,28 +38,77 @@ class _SignInState extends State<SignIn> {
           ),
           Padding(
             padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                Input(
-                  input: AppColors.input,
-                  colorPlaceholder: AppColors.inputPlaceholder,
-                  primaryColor: AppColors.primaryColor,
-                  labelText: 'Your email',
-                  prefixIcon: Icons.mail_outline,
-                ),
-                Input(
-                  obscureText: true,
-                  input: AppColors.input,
-                  colorPlaceholder: AppColors.inputPlaceholder,
-                  primaryColor: AppColors.primaryColor,
-                  labelText: 'Your password',
-                  prefixIcon: Icons.lock_outline,
-                ),
-                Button(
-                  textButton: 'Entrar',
-                  backgroundButton: AppColors.primaryColor,
-                ),
-              ],
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Input(
+                    controllerInput: _emailController,
+                    input: AppColors.input,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a valid email address.';
+                      }
+                      return null;
+                    },
+                    colorPlaceholder: AppColors.inputPlaceholder,
+                    primaryColor: AppColors.primaryColor,
+                    labelText: 'Your email',
+                    prefixIcon: Icons.mail_outline,
+                    errorText: _emailErrorText,
+                  ),
+                  Input(
+                    controllerInput: _passwordController,
+                    obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a valid password.';
+                      }
+                      return null;
+                    },
+                    input: AppColors.input,
+                    colorPlaceholder: AppColors.inputPlaceholder,
+                    primaryColor: AppColors.primaryColor,
+                    labelText: 'Your password',
+                    prefixIcon: Icons.lock_outline,
+                    errorText: _passwordErrorText,
+                  ),
+                  Button(
+                    textButton: 'Entrar',
+                    backgroundButton: AppColors.primaryColor,
+                    onPressCallback: () async {
+                      if (_formKey.currentState != null &&
+                          _formKey.currentState!.validate()) {
+                        if (_emailController.text.isNotEmpty &&
+                            _passwordController.text.isNotEmpty) {
+                          setState(() {
+                            _isLoading = true;
+                          });
+                          await Future.delayed(const Duration(seconds: 2));
+
+                          Navigator.pushNamed(context, '/home').then((_) {
+                            setState(() {
+                              _isLoading = false;
+                            });
+                            clearFields(
+                              emailController: _emailController,
+                              passwordController: _passwordController,
+                              setStateCallback: setState,
+                            );
+                          });
+
+                          FocusScope.of(context).requestFocus(FocusNode());
+                        }
+                      } else {
+                        setState(() {
+                          _isLoading = false;
+                        });
+                      }
+                    },
+                    isLoading: _isLoading,
+                  ),
+                ],
+              ),
             ),
           ),
           Footer(
--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:textfield/constants/app_colors.dart';
@@ -6,83 +8,193 @@ import 'package:textfield/widgets/footer.dart';
 import 'package:textfield/widgets/input.dart';
 import 'package:textfield/widgets/big_title.dart';
 
+import '../utils/utils.dart';
 import '../widgets/button.dart';
 
 class SignUp extends StatefulWidget {
-  const SignUp({super.key});
+  const SignUp({Key? key}) : super(key: key);
 
   @override
   State<SignUp> createState() => _SignUpState();
 }
 
 class _SignUpState extends State<SignUp> {
-  bool isChecked = false;
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _usernameController = TextEditingController();
+
+  String? _emailErrorText;
+  String? _passwordErrorText;
+  String? _usernameErrorText;
+  bool _isChecked = false;
+  bool _isTermsCheckedError = false;
+  final String _termsErrorText = 'Please accept the terms.';
+
+  bool _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: HexColor(AppColors.background),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          BigTitle(
-            title: 'Sing Up',
-            colorText: AppColors.primaryColor,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                Input(
-                  primaryColor: AppColors.primaryColor,
-                  labelText: 'Your email',
-                  colorPlaceholder: AppColors.inputPlaceholder,
-                  prefixIcon: Icons.mail_outline,
-                  input: AppColors.input,
-                ),
-                Input(
-                  primaryColor: AppColors.primaryColor,
-                  labelText: 'username',
-                  colorPlaceholder: AppColors.inputPlaceholder,
-                  prefixIcon: Icons.person_outline_outlined,
-                  input: AppColors.input,
-                ),
-                Input(
-                  primaryColor: AppColors.primaryColor,
-                  labelText: 'Your password',
-                  colorPlaceholder: AppColors.inputPlaceholder,
-                  prefixIcon: Icons.lock_outline,
-                  input: AppColors.input,
-                ),
-                Button(
-                  textButton: 'create account',
-                  backgroundButton: AppColors.primaryColor,
-                ),
-                Row(
-                  children: [
-                    Checkbox(
-                        value: isChecked,
-                        onChanged: (value) {
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              BigTitle(
+                title: 'Sing Up',
+                colorText: AppColors.primaryColor,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Input(
+                        controllerInput: _emailController,
+                        primaryColor: AppColors.primaryColor,
+                        labelText: 'Your email',
+                        colorPlaceholder: AppColors.inputPlaceholder,
+                        prefixIcon: Icons.mail_outline,
+                        input: AppColors.input,
+                        errorText: _emailErrorText,
+                        validator: (value) {
                           setState(() {
-                            isChecked = value!;
+                            _emailErrorText = value!.isEmpty
+                                ? 'Please enter a valid email address.'
+                                : null;
                           });
-                        }),
-                    TermsAndConditions(),
-                  ],
+                          return null;
+                        },
+                      ),
+                      Input(
+                        controllerInput: _usernameController,
+                        primaryColor: AppColors.primaryColor,
+                        labelText: 'username',
+                        colorPlaceholder: AppColors.inputPlaceholder,
+                        prefixIcon: Icons.person_outline_outlined,
+                        input: AppColors.input,
+                        errorText: _usernameErrorText,
+                        validator: (value) {
+                          setState(() {
+                            _usernameErrorText = value!.isEmpty
+                                ? 'Please enter a valid username.'
+                                : null;
+                          });
+                          return null;
+                        },
+                      ),
+                      Input(
+                        controllerInput: _passwordController,
+                        primaryColor: AppColors.primaryColor,
+                        labelText: 'Your password',
+                        colorPlaceholder: AppColors.inputPlaceholder,
+                        prefixIcon: Icons.lock_outline,
+                        input: AppColors.input,
+                        errorText: _passwordErrorText,
+                        validator: (value) {
+                          setState(() {
+                            _passwordErrorText = value!.isEmpty
+                                ? 'Please enter a valid password.'
+                                : null;
+                          });
+                          return null;
+                        },
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Checkbox(
+                                fillColor:
+                                    MaterialStatePropertyAll(Colors.white),
+                                checkColor: Colors.amber,
+                                value: _isChecked,
+                                onChanged: (value) {
+                                  setState(() {
+                                    _isChecked = value!;
+                                    _isTermsCheckedError = false;
+                                  });
+                                },
+                              ),
+                              TermsAndConditions(),
+                            ],
+                          ),
+                          if (_isTermsCheckedError)
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 14.0), // Ajusta o recuo do texto
+                              child: Text(
+                                _termsErrorText,
+                                style: TextStyle(
+                                  color: HexColor(AppColors.warning),
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      Button(
+                        onPressCallback: () async {
+                          setState(() {
+                            _isTermsCheckedError = !_isChecked;
+                          });
+
+                          if (_formKey.currentState != null &&
+                              _formKey.currentState!.validate() &&
+                              _emailErrorText == null &&
+                              _usernameErrorText == null &&
+                              _passwordErrorText == null) {
+                            if (!_isTermsCheckedError) {
+                              setState(() {
+                                _isLoading = true;
+                              });
+
+                              await Future.delayed(const Duration(seconds: 2));
+
+                              Navigator.pushNamed(context, '/home').then((_) {
+                                setState(() {
+                                  _isLoading = false;
+                                });
+
+                                if (_isChecked) {
+                                  clearFields(
+                                    emailController: _emailController,
+                                    passwordController: _passwordController,
+                                    setStateCallback: setState,
+                                    usernameController: _usernameController,
+                                    checkboxValue: _isChecked = false,
+                                  );
+                                }
+                              });
+                            }
+                          }
+                        },
+                        textButton: 'create account',
+                        backgroundButton: AppColors.primaryColor,
+                        isLoading: _isLoading,
+                      ),
+                    ],
+                  ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-          Footer(
-            beforeText: 'Already have an account?',
-            linkText: 'Sign In',
-            linkTextColor: AppColors.primaryColor,
-            dividerColor: AppColors.primaryColor,
-            onTapRoute: () {
-              Navigator.pushNamed(context, '/');
-            },
-          )
-        ],
+        ),
+      ),
+      bottomNavigationBar: Footer(
+        beforeText: 'Already have an account?',
+        linkText: 'Sign In',
+        linkTextColor: AppColors.primaryColor,
+        dividerColor: AppColors.primaryColor,
+        onTapRoute: () {
+          Navigator.pushNamed(context, '/');
+        },
       ),
     );
   }
